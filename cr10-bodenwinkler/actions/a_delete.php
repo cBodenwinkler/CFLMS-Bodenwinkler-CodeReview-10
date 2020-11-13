782ed2e6cc3253e1595dddb90ff3d81352bd04ea
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-11-13 14:45:06 +0100

    Basic mostly done, More Entries, Design and Bonus1 ToDo

---
+++ cr10-bodenwinkler/actions/a_delete.php
@@ -0,0 +1,21 @@
+<?php 
+    require_once 'db_connect.php';
+
+    if($_POST) {
+        $id = $_POST['id'];
+
+        $sql = "DELETE FROM media WHERE id = {$id}";
+        
+        if($connect->query($sql)===TRUE) {
+            echo "<p>Successfully deleted!!</p>";
+            echo "<a href='../index.php'><button type='button'>Back</button></a>";
+        }else{
+            echo "Error while deleting Entry: " . $connect->error;
+        }
+        $connect->close();
+    }
+
+
+
+
+?>
